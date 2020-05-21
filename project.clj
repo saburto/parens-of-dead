8384d89d923d3ba10eabcf204ae2a7d31ae7b7bb
Sebastian Aburto <seba.aburto@gmail.com>
2020-05-21 13:03:06 +0200

    Add http server

--- project.clj
+++ project.clj
@@ -1,9 +1,14 @@
 (defproject parens-of-dead "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Parens of dead game practice for clojure"
+  :url "http://github.com/saburto/parens-of-dead"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.1"]]
-  :main ^:skip-aot parens-of-dead.core
+  :main undead.system
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [http-kit "2.3.0"]
+                 [com.stuartsierra/component "1.0.0"]
+                 ]
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :profiles {:dev {:plugins []
+                   :dependencies [[reloaded.repl "0.2.4"]]
+                   :source-paths ["dev"]}})
