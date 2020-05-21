b146a1bbbbbd642bfc0bb3197a68de93f35ed358
Sebastian Aburto <seba.aburto@gmail.com>
2020-05-21 11:23:30 +0200

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to parens-of-dead
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/parens_of_dead/core_test.clj
@@ -0,0 +1,7 @@
+(ns parens-of-dead.core-test
+  (:require [clojure.test :refer :all]
+            [parens-of-dead.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
