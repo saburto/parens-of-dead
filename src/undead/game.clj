ecf20d46f587e3d10c324c73dc8ba40efa34d0d9
Sebastian Aburto <seba.aburto@gmail.com>
2020-05-24 19:20:00 +0200

    Refactor mapv

--- src/undead/game.clj
+++ src/undead/game.clj
@@ -7,6 +7,9 @@
 (defn- ->tile [face]
   {:face face})
 
+(defn- update-tiles [game f]
+  (update-in game [:tiles] #(mapv f %)))
+
 (defn create-game []
   {:tiles (shuffle (map ->tile faces))
    :sand (repeat 30 :remaining)
@@ -18,14 +21,12 @@
 (defn- can-reveal? [game]
   (> 2 (count (revealed-tiles game))))
 
-(defn- match-reveal [tiles]
-  (mapv (fn [tile]
-          (if (:revealed? tile)
-            (-> tile
-                (assoc :matched? true)
-                (dissoc :revealed?))
-            tile
-            )) tiles))
+(defn- match-reveal [tile]
+  (if (:revealed? tile)
+    (-> tile
+        (assoc :matched? true)
+        (dissoc :revealed?))
+    tile))
 
 (defn- get-match [game]
   (let [rtiles (revealed-tiles game)]
@@ -40,38 +41,34 @@
    (->> (drop-while (complement #{:remaining}) sand)
         (drop (count replacement)))))
 
-(defn- wake-the-dead [tiles]
-  (mapv (fn[tile]
-          (if (= :gy (:face tile))
-            (assoc tile :face :zo)
-            tile))
-        tiles))
+(defn- wake-the-dead [tile]
+  (if (= :gy (:face tile))
+    (assoc tile :face :zo)
+    tile))
 
 (defn- perfom-match-actions [game match]
   (case match
     :fg (assoc game :foggy? true)
     :zo (-> game
             (update-in [:sand] #(replace-remaining % (repeat 3 :zoombie)))
-            (update-in [:tiles] wake-the-dead))
+            (update-tiles wake-the-dead))
     game))
 
 (defn- check-for-match [game]
   (if-let [match (get-match game)]
     (-> game
-        (update-in [:tiles] match-reveal)
+        (update-tiles match-reveal)
         (perfom-match-actions match))
       game))
 
-(defn- init-cancelment [tiles]
-  (mapv (fn [tile]
-          (if (:revealed? tile)
-            (assoc tile :canceal-countdown 3)
-            tile))
-        tiles))
+(defn- init-cancelment [tile]
+  (if (:revealed? tile)
+    (assoc tile :canceal-countdown 3)
+    tile))
 
 (defn- check-for-canelment [game]
   (if-not (can-reveal? game)
-    (update-in game [:tiles] init-cancelment)
+    (update-tiles game init-cancelment)
     game))
 
 (defn reveal-tile [game index]
@@ -85,27 +82,23 @@
 (defn- assoc-ids [tiles]
   (map-indexed #(assoc %2 :id %1) tiles))
 
-(defn- hide-faces [tiles]
-  (mapv (fn [tile]
-          (if (or (:revealed? tile)
-                  (:matched? tile))
-            tile
-            (dissoc tile :face)))
-        tiles))
+(defn- hide-faces [tile]
+  (if (or (:revealed? tile)
+          (:matched? tile))
+    tile
+    (dissoc tile :face)))
 
 (defn prep [game]
   (-> game
       (update-in [:tiles] assoc-ids)
-      (update-in [:tiles] hide-faces)))
+      (update-tiles hide-faces)))
 
-(defn- cancel-faces [tiles]
-  (mapv (fn [tile]
-          (case (:canceal-countdown tile)
-            nil tile
-            1 (dissoc tile :canceal-countdown :revealed?)
-            (update tile :canceal-countdown dec)))
-        tiles))
+(defn- cancel-faces [tile]
+  (case (:canceal-countdown tile)
+    nil tile
+    1 (dissoc tile :canceal-countdown :revealed?)
+    (update tile :canceal-countdown dec)))
 
 (defn tick [game]
   (-> game
-      (update-in [:tiles] cancel-faces)))
+      (update-tiles cancel-faces)))
